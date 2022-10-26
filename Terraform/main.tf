91fdcf3d46622c007718c4ff9fc146c9ee28f258
PoPi2 <guy.scher@gmail.com>
2022-10-26 19:33:14 +0300

    exposed port 8080

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -76,6 +76,13 @@ resource "aws_security_group" "allow_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    description = "HTTP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     description = "SSH"
     from_port   = 22
     to_port     = 22
